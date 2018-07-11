c05afd7594515fd76e39ee639d1106bb8760cb5b
Drake Morrison <drakermorrison@gmail.com>
2018-07-11 18:48:22 -0500

    added components and routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,112 @@
 import React, { Component } from 'react';
+import {Route, BrowserRouter, Redirect, Switch}  from 'react-router-dom';
+import firebase from 'firebase';
+
 import './App.css';
+import Home from '../components/Home/Home';
+import Navbar from '../components/Navbar/Navbar';
+import AllStuff from '../components/AllStuff/AllStuff';
+import MyStuff from '../components/MyStuff/MyStuff';
+import Login from '../components/Login/Login';
+import Register from '../components/Register/Register';
+import fbConnection from '../firebaseRequests/connection';
+fbConnection();
+
+const PrivateRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
+const PublicRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/orders', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
 
 class App extends Component {
+  state={
+    authed: false,
+  };
+
+  componentDidMount () {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({authed: true});
+      } else {
+        this.setState({authed: false});
+      }
+    });
+  }
+
+  componentWillUnmount () {
+    this.removeListener();
+  }
+
+  runAway = () => {
+    this.setState({authed: false});
+  }
+
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+          <div>
+            <Navbar
+              authed={this.state.authed}
+              runAway={this.runAway}
+            />
+            <div className='container'>
+              <div className='row'>
+                <Switch>
+                  <Route path='/' exact component={Home}/>
+                  <PublicRoute
+                    path='/register'
+                    authed={this.state.authed}
+                    component={Register}
+                  />
+                  <PublicRoute
+                    path='/login'
+                    authed={this.state.authed}
+                    component={Login}
+                  />
+                  <PrivateRoute
+                    path='/mystuff'
+                    authed={this.state.authed}
+                    component={MyStuff}
+                  />
+                  <PrivateRoute
+                    path='/allstuff'
+                    authed={this.state.authed}
+                    component={AllStuff}
+                  />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  render () {
+    return (
+      <div className='Navbar'>
+        <h2>Navbar</h2>
+      </div>
+    );
+  };
+};
+
+export default Navbar;
