1bcb0f2734345ef2124b9cca1b69e99b2954cdac
Drake Morrison <drakermorrison@gmail.com>
2018-07-11 19:23:01 -0500

    axios; allstuff; item.js

--- src/components/AllStuff/AllStuff.js
+++ src/components/AllStuff/AllStuff.js
@@ -1,13 +1,42 @@
 import React from 'react';
 
+import allStuffRequests from '../../firebaseRequests/allStuff';
+import Item from '../Item/Item';
+
+import './AllStuff.css';
+
 class AllStuff extends React.Component {
+  state = {
+    stuff: [],
+  }
+
+  componentDidMount () {
+    allStuffRequests
+      .getRequest()
+      .then((stuff) => {
+        this.setState({stuff});
+      })
+      .catch(console.error.bind(console));
+  }
+
   render () {
+    const itemComponents = this.state.stuff.map((item) => {
+      return (
+        <Item
+          key={item.id}
+          details={item}
+        />
+      );
+    });
     return (
-      <div className='AllStuff'>
+      <div className="AllStuff col-xs-12">
         <h2>AllStuff</h2>
+        <ul className="fishes">
+          {itemComponents}
+        </ul>
       </div>
     );
-  };
-};
+  }
+}
 
 export default AllStuff;
